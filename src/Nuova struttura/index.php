7cae858e0a73f51f8fb189a17a12af62990c3469
FilippoGurioli <93600198+FilippoGurioli@users.noreply.github.com>
2023-01-26 17:45:17 +0100

    Merge conflict

--- src/Nuova struttura/index.php
+++ src/Nuova struttura/index.php
@@ -5,11 +5,7 @@ require_once 'bootstrap.php';
 $templateParams["titolo"] = "WeFit";
 $templateParams["nome"] = "register.php";
 $templateParams["js"] = array("https://unpkg.com/axios/dist/axios.min.js","js/register.js");
-<<<<<<< HEAD
 $templateParams["css"] = array(/*"create-post", "dark-background", */"light-background", /*"login", */"navbar", /*"personal-profile", "post",*/ "register", "register-login"/*, "settings"*/);
-=======
-$templateParams["css"] = array("create-post", "dark-background", "light-background", "login", "navbar", "personal-profile", "post", "register", "register-login", "settings");
->>>>>>> 400e936fca3b44d3915aefa657abdaab76da994b
 
 require 'template/base.php';
 ?>
