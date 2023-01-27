ffcc2b1ac4f64c0b1939af010d4a03e2de6da1a0
Silvia Furegato <silvia.furegato@studio.unibo.it>
2023-01-27 16:16:13 +0100

    Settings fix

---
+++ src/js/settings.js
@@ -0,0 +1,17 @@
+document.querySelector("main form").addEventListener("submit", function (event) {
+	event.preventDefault();
+	const username = document.querySelector("input[name=username]").value;
+	const password = document.querySelector("input[name=password]").value;
+	login(username, password);
+});
+
+function login(username, password) {
+    const formData = new FormData();
+    formData.append('username', username);
+    formData.append('password', password);
+    axios.post('api-login.php', formData).then(response => {
+		if(response.data["logineseguito"]) {
+			window.location.pathname = './ProgettoWeb-WeFit/src/profile-redirector.php';
+		}
+    });
+}
