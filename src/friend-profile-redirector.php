3c8b52ada1f10d976562bf1a4ddda663ab8cfeef
Filippo Gurioli <filippo.gurioli@studio.unibo.it>
2023-01-27 16:30:26 +0100

    Refactor personal profile into profile

--- src/friend-profile-redirector.php
+++ src/friend-profile-redirector.php
@@ -2,8 +2,8 @@
 require_once 'bootstrap.php';
 
 $templateParams["titolo"] = "WeFit - Friend Profile";
-$templateParams["nome"] = "personalprofile.php";
-$templateParams["css"] = array("dark-background", "navbar", "post", "personal-profile");
+$templateParams["nome"] = "profile.php";
+$templateParams["css"] = array("dark-background", "navbar", "post", "profile");
 $templateParams["js"] = array("https://unpkg.com/axios/dist/axios.min.js","js/search.js");
 $navbarVisible = true;
 $isPersonalP = false;
--- src/profile-redirector.php
+++ src/profile-redirector.php
@@ -2,8 +2,8 @@
 require_once 'bootstrap.php';
 
 $templateParams["titolo"] = "WeFit - Profile";
-$templateParams["nome"] = "personalprofile.php";
-$templateParams["css"] = array("dark-background", "navbar", "post", "personal-profile");
+$templateParams["nome"] = "profile.php";
+$templateParams["css"] = array("dark-background", "navbar", "post", "profile");
 $templateParams["js"] = array("https://unpkg.com/axios/dist/axios.min.js","js/search.js");
 $navbarVisible = true;
 $isPersonalP = true;
