4d2728a8965ea5c7df865c4adec63d4a4b949a7c
Silvia Furegato <silvia.furegato@studio.unibo.it>
2023-01-28 14:38:41 +0100

    Updated database

--- src/db/database.php
+++ src/db/database.php
@@ -126,5 +126,15 @@
         return $result->fetch_all(MYSQLI_ASSOC);
 		}
 		
+		public function getFollowing($follower) {
+			$query = "SELECT User2 FROM `followed` WHERE `User1`=?";
+			$stmt = $this->db->prepare($query);
+			$stmt->bind_param('s', $follower);
+			$stmt->execute();
+			$result = $stmt->get_result();
+
+			return $result->fetch_all(MYSQLI_ASSOC);
+		}
+		
     }
 ?>
