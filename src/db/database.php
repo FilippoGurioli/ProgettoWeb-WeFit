949ace7dc5b5f2c958c7d8407d475f6af940383a
FilippoGurioli <93600198+FilippoGurioli@users.noreply.github.com>
2023-01-29 12:38:16 +0100

    Silvia Furegato: modified date format

--- src/db/database.php
+++ src/db/database.php
@@ -11,7 +11,9 @@
 		
 		public function register($user, $birth, $email, $height, $weight, $password) {
             $stmt = $this->db->prepare("INSERT INTO `users` (`Username`, `Email`, `Password`, `Photo`, `Birthday`, `Height`, `Weight`) VALUES (?, ?, ?, './upload/profile_pictures/WeFitPic.png', ?, ?, ?);");
-            $stmt->bind_param('sssidd', $user, $email, $password, $birth, $height, $weight);
+            $birth = strtotime($birth);
+			$birthSQL = date('Y-m-d', $birth);
+			$stmt->bind_param('ssssdd', $user, $email, $password, $birthSQL, $height, $weight);
             $stmt->execute();
 		}
 		
